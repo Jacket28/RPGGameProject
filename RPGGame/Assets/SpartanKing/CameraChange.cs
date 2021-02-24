da3ba6df3c87fd094e42e7c600b814e27485c5bd
Jacket28 <48347504+Jacket28@users.noreply.github.com>
2021-02-24 21:13:54 +0100

    Sprint 0 v1.0
    - Creation of a Terrain
    - Creation of a character
    - Implementation of walking for the character
    - Implementation of running for the character
    - Implementation of idling for the character
    - Implementation of jumping for the character
    - Implementation of 1st and 3rd person view

---
+++ RPGGame/Assets/SpartanKing/CameraChange.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+
+    public GameObject thirdCam;
+    public GameObject firstCam;
+    public int camMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camMode = 1;
+        thirdCam.SetActive(false);
+        firstCam.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Camera"))
+        {
+            if (camMode == 1)
+            {
+                camMode = 0;
+            }
+            else
+            {
+                camMode += 1;
+            }
+        }
+        
+        StartCoroutine(CamChange());
+
+    }
+    IEnumerator CamChange()
+    {
+        yield return new WaitForSeconds(0.01f);
+        if (camMode == 0)
+        {
+            thirdCam.SetActive(true);
+            firstCam.SetActive(false);
+        }
+        if (camMode == 1)
+        {
+            firstCam.SetActive(true);
+            thirdCam.SetActive(false);
+        }
+
+    }
+}
+
---
+++ RPGGame/Assets/SpartanKing/CharacterMotor.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotor : MonoBehaviour
+{
+
+    Animation animations;
+
+    public float walkSpeed;
+    public float runSpeed;
+    public float turnSpeed;
+
+    public string inputFront;
+    public string inputBack;
+    public string inputLeft;
+    public string inputRight;
+
+    public Vector3 jumpSpeed;
+    CapsuleCollider playerCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animations = gameObject.GetComponent<Animation>();
+        playerCollider = gameObject.GetComponent<CapsuleCollider>();
+
+
+    }
+
+    bool IsGrounded()
+    {
+        Vector3 dwn = transform.TransformDirection(Vector3.down);
+
+        return (Physics.Raycast(transform.position, dwn, 1));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(inputFront)&& !Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(0,0, walkSpeed * Time.deltaTime);
+            animations.Play("walk");
+        }
+
+        if (Input.GetKey(inputBack))
+        {
+            transform.Translate(0, 0, -(walkSpeed/2) * Time.deltaTime);
+            animations.Play("walk");
+        }
+
+        if (Input.GetKey(inputLeft))
+        {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(inputRight))
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(inputFront)&& Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(0, 0, runSpeed * Time.deltaTime);
+            animations.Play("run");
+        }
+
+        if (!Input.GetKey(inputBack) && !Input.GetKey(inputFront))
+        {
+            animations.Play("idle");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Vector3 v = gameObject.GetComponent<Rigidbody>().velocity;
+            v.y = jumpSpeed.y;
+
+            gameObject.GetComponent<Rigidbody>().velocity = jumpSpeed;
+        }
+
+
+    }
+}
