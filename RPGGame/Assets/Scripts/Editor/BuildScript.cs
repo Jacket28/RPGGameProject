98a6136873a64b9860afa44b11ca6f6e4ad4a371
Jacket28 <48347504+Jacket28@users.noreply.github.com>
2021-03-02 15:58:40 +0100

    test
    test

---
+++ RPGGame/Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScript
+{
+    static void PerformBuild()
+    {
+        string[] defaultScene = { "Assets/Scenes/MainScene.Unity" };
+        BuildPipeline.BuildPlayer(defaultScene, "./builds/RPGGame.exe", 
+            BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+}
